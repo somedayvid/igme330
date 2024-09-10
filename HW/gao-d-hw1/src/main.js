8a7b66b033e262dfc19e3210b84693d82f449572
David Gao <david.04.gao@gmail.com>
2024-09-10 13:30:04 -0400

    initial commit copy over pe1 to hw1

---
+++ HW/gao-d-hw1/src/main.js
@@ -0,0 +1,23 @@
+"use strict";
+            
+            const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
+            
+            const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
+            
+            const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
+
+            function getRand(wordsArray){
+                return wordsArray[Math.floor(Math.random() * wordsArray.length)];
+            }
+
+            function generateBabble(){
+                document.querySelector("#output").innerHTML = getRand(words1) + " " + getRand(words2) + " " + getRand(words3);
+            }
+
+            function init(){
+                const generateButton = document.querySelector("#myButton");
+                generateButton.addEventListener("click", generateBabble);
+                generateBabble();
+            }
+
+            window.onload = init;
