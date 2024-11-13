a9f695b3dfe22286a468aef64272918d8549e48f
David Gao <david.04.gao@gmail.com>
2024-11-13 00:48:31 -0500

    bulma done

--- HW/gao-d-h3-refactor/src/main.js
+++ HW/gao-d-h3-refactor/src/main.js
@@ -1,256 +1,340 @@
 import * as utils from './utils.js';
-import * as audio from './audio.js';
-import * as canvas from './visualizer.js'
-import * as game from './gameManager.js'
-import { readAppData } from './dataReader.js';
+import * as info from './extra-info.js';
 
-export let dataType;
-export let paused = false;
-export let autoplay = false;
+let mainKanjiArray = [];
+let mainVocabArray = [];
+let mainRadicalsArray = [];
+let term = "";
+let timesToLoop = 0;
 
-let songSelectionData = {};
-
-let titleGone = false;
-let titleAlpha = 1;
-
-let isKeysPressed = {
-  s: {
-    justPressed: false,
-    pressed: false,
-    released: true,
-    number: 0
-  },
-  d: {
-    justPressed: false,
-    pressed: false,
-    released: true,
-    number: 1
-  },
-  j: {
-    justPressed: false,
-    pressed: false,
-    released: true,
-    number: 2
-  },
-  k: {
-    justPressed: false,
-    pressed: false,
-    released: true,
-    number: 3
-  }
-};
-
-let textInfoBox;
-
-const drawParams = {
-  showLine : true,
-  showBars : true,
-  showCircle : true,
-  showNotes : true,
-  showCircleBars : true
-};
-
-// 1 - here we are faking an enumeration
-const DEFAULTS = Object.freeze({
-	bass  : "media/Taking What's Not Yours - TV Girl/bass.mp3",
-  drums : "media/Taking What's Not Yours - TV Girl/drums.mp3",
-  instrumental: "media/Taking What's Not Yours - TV Girl/instrumental.mp3",
-  other: "media/Taking What's Not Yours - TV Girl/other.mp3",
-  fullSong: "media/Taking What's Not Yours - TV Girl/original.mp3"
+const requestHeaders =
+new Headers({
+  Authorization: 'Bearer ' +  ' 89abe689-ce3d-4035-acfd-65d442782f72 ',
 });
+ 
+//local storage variables
+const prefix = "dg8516-";
+const searchTermKey = prefix + "term";
+const wordTypeKey = prefix + "type";
+const searchByKey = prefix + "searchBy";
 
-const init = () => {
-  audio.setupWebaudio(DEFAULTS.bass,DEFAULTS.drums,DEFAULTS.intrumental,DEFAULTS.other, DEFAULTS.fullSong);
-	let canvasElement = document.querySelector("canvas"); 
-  const handleData = (data) => {  
-    songSelectionData = data;
-    textInfoBox = document.querySelector("#text-info");
-    textInfoBox.firstElementChild.innerHTML = songSelectionData.title;
-    textInfoBox.lastElementChild.innerHTML = songSelectionData.instructions;
-    document.querySelector('title').textContent = songSelectionData.title;
-};
+const storedTerm = localStorage.getItem(searchTermKey);
+const storedType = localStorage.getItem(wordTypeKey);
+const storedSearchBy = localStorage.getItem(searchByKey);
 
-// Call `readAppData` and pass the callback function
-  readAppData(handleData);
-	setupUI(canvasElement);
-  canvas.setupCanvas(canvasElement,audio.audioList, audio.fullSong);
-  loop();
-}
+window.onload = () => {
+  const searchWindow = document.querySelector("#search-term");
+  const typeSelector = document.querySelector("#type");
+  const searchBySelector = document.querySelector("#search-by");
 
-const setupUI = canvasElement =>{
-  // A - hookup fullscreen button
-  const fsButton = document.querySelector("#fs-button");
-	
-  // add .onclick event to button
-  fsButton.onclick = e => {
-    utils.goFullscreen(canvasElement);
-  };
-	
-  //HERE IS A SPOT TO MAKE WORK WITH OTHER SPOTS
-  //add .onclick even to button
-  const playButton = document.querySelector("#play-button");
-  playButton.onclick = e => {
-    //check if context is in suspended state (autoplay policy)
-    if(audio.audioList[0].audioCtx.state == "suspended"){
-        audio.audioList[0].audioCtx.resume();
-        audio.fullSong.audioCtx.resume();
-    }
-    if(e.target.dataset.playing == "no"){
-        // if track is currently paused, play it
-        audio.playCurrentSound();
-        e.target.dataset.playing = "yes"; //our css will set the text to "Pause"
-        paused = false;
-    }
-    //if track IS playing, pause it
-    else{
-        audio.pauseCurrentSound();
-        e.target.dataset.playing = "no";// our CSS will set the text to "Play"
-        paused = true;
-    }
+  //attaches const to actions the user can take
+  document.querySelector("#search").onclick = searchButtonClicked;
 
-    if(!titleGone){
-      ridTitle();
-    }
-  };
+  info.showHomeInfo();
+  document.querySelector("#title").onclick = info.showHomeInfo;
+  searchWindow.onchange = storeAll;
+  typeSelector.onchange = storeAll;
+  searchBySelector.onchange = storeAll;
+  typeSelector.onchange = displayExplanation;
 
-  // C - hookup volume slider and label
-  let volumeSlider = document.querySelector("#volume-slider");
-
-  //add .oninput event to slider
-  volumeSlider.oninput = e => {
-    //set the gain
-    audio.setVolume(e.target.value);
-    //update value of label to match value of slider
-    document.querySelector("#volume-label").innerHTML = `Volume: ${Math.round((e.target.value/2 * 100))}`;
-  };
-
-  //set value of label to match innitial value of slider
-  volumeSlider.dispatchEvent(new Event("input"));
-
-  //D - hookup track <select>
-  let trackSelect = document.querySelector("#track-select");
-
-  document.querySelector("#default-track").selected = true;
-
-  //add .onchange event to <select>
-  trackSelect.onchange = e => {
-    audio.loadBeatsAndSong(songSelectionData.songs[e.target.value]);
-    canvas.clearAll();
-    //pause the current track if it is playing
-    if(playButton.dataset.playing == "yes"){
-        playButton.dispatchEvent(new MouseEvent("click"));
-    }
-  };
-
-  const dataTypeSelect = document.querySelector("#type-data-select");
-  dataType = dataTypeSelect.value = "frequency";
-  dataTypeSelect.onchange = () =>{
-    dataType = dataTypeSelect.value;
-    if(!titleGone){
-      ridTitle();
-    }
-  }
-
-  const lineCheckBox = document.querySelector("#line-CB");
-  const barsCheckBox = document.querySelector("#bars-CB");
-  const circleCheckBox = document.querySelector("#circle-CB");
-  const notesCheckBox = document.querySelector("#notes-CB");
-  const circleBarsCheckBox = document.querySelector("#circle-bars-CB");
-
-  const autoplayCheckBox = document.querySelector("#autoplay-CB");
-
-  lineCheckBox.checked = drawParams.showLine;
-  barsCheckBox.checked = drawParams.showBars;
-  circleCheckBox.checked = drawParams.showCircle;
-  notesCheckBox.checked = drawParams.showNotes;
-  circleBarsCheckBox.checked = drawParams.showCircleBars;
-  autoplayCheckBox.checked = autoplay;
-
-  lineCheckBox.onchange = () =>{
-    drawParams.showLine = lineCheckBox.checked;
-  }
-  barsCheckBox.onchange = () =>{
-    drawParams.showBars = barsCheckBox.checked;
-  }
-  circleCheckBox.onchange = () =>{
-    drawParams.showCircle = circleCheckBox.checked;
-  }
-  notesCheckBox.onchange = () =>{
-    drawParams.showNotes = notesCheckBox.checked;
-  }
-  circleBarsCheckBox.onchange = () =>{
-    drawParams.showCircleBars = circleBarsCheckBox.checked;
-  }
-
-  autoplayCheckBox.onchange = () =>{
-    autoplay = autoplayCheckBox.checked;
-  }
-
-  let boostSlider = document.querySelector("#bass-boost-slider");
-
-  //add .oninput event to slider
-  boostSlider.oninput = e => {
-    //set the gain
-    audio.bassBoost(e.target.value);
-    //update value of label to match value of slider
-    document.querySelector("#bass-label").innerHTML = `Bass Boost Level: ${e.target.value}`;
-  };
-  //set value of label to match innitial value of slider
-  boostSlider.dispatchEvent(new Event("input"));
-
-  let trebleSlider = document.querySelector("#treble-boost-slider");
-
-  //add .oninput event to slider
-  trebleSlider.oninput = e => {
-    //set the gain
-    audio.trebleBoost(e.target.value);
-    //update value of label to match value of slider
-    document.querySelector("#treble-label").innerHTML = `Treble Boost Level: ${e.target.value}`;
-  };
-  //set value of label to match innitial value of slider
-  trebleSlider.dispatchEvent(new Event("input"));
-
-  document.addEventListener('keydown', (e) => {
-    if(e.key == 's' || e.key == 'd' || e.key == 'j' || e.key == 'k')
-    {
-      isKeysPressed[e.key].pressed = true;
-      if(isKeysPressed[e.key].released){
-        isKeysPressed[e.key].justPressed = true;
-        isKeysPressed[e.key].released = false;
-        setTimeout(() => {
-          isKeysPressed[e.key].justPressed = false;
-        }, 25);        
-      }
-    }
-  })
-
-  document.addEventListener('keyup', (e) => {
-    if(e.key == 's' || e.key == 'd' || e.key == 'j' || e.key == 'k'){
-      isKeysPressed[e.key].pressed = false;
-      isKeysPressed[e.key].released = true;
-    } 
-  })
-} // end setupUI
-export {init, isKeysPressed};
-
-const loop = () =>{
-  setTimeout(loop, 1000/60);
-  if(!paused){
-    game.update();
-    canvas.draw(drawParams);
-  }
-}
-
-const ridTitle = () =>{
-  textInfoBox.style.color = `rgba(255,255,255,${titleAlpha})`;
-  titleAlpha *= .9;
-  if(titleAlpha > .001){
-    setTimeout(() => {
-      ridTitle();
-    }, 1000/60);
+  //checks and puts stored terms into each interactible item
+  if(storedTerm){
+    searchWindow.value = storedTerm;
   }
   else{
-    textInfoBox.firstElementChild.innerHTML = "";
-    textInfoBox.lastElementChild.innerHTML = "";
+    searchWindow.value = "summer";
   }
+  if(storedType){
+    typeSelector.value = storedType;
+  }
+  else{
+    typeSelector.value = "";
+  }
+  if(storedSearchBy){
+    searchBySelector.value = storedSearchBy;
+  }
+  else{
+    searchBySelector.value = "";
+  }
+
+  //gets all api information for japanese writing systems
+  getAllWords("radical",'https://api.wanikani.com/v2/subjects?types=radical');
+  getAllWords("kanji", 'https://api.wanikani.com/v2/subjects?types=kanji');
+  getAllWords("vocabulary", 'https://api.wanikani.com/v2/subjects?types=vocabulary');
+};
+
+//is called each time a locally stored value is changed to ensure all changes are accounted for
+const storeAll = () => {
+  localStorage.setItem(searchTermKey, document.querySelector("#search-term").value);
+  localStorage.setItem(wordTypeKey, document.querySelector("#type").value);
+  localStorage.setItem(searchByKey, document.querySelector("#search-by").value);
+}
+
+//gets the button search options and passes them into the data accessor
+const searchButtonClicked = () => {
+  const searchType = document.querySelector("#type").value;
+  const searchBy = document.querySelector("#search-by").value;
+  const resultsInfo = document.querySelector("#num-results");
+  term = document.querySelector("#search-term").value;
+
+  document.querySelector("#display").innerHTML = "";
+
+  term = term.trim();
+
+  //checks if the search term even exists first, and gives user feedback
+  if(term == ""){
+    if(searchBy == "level"){
+      resultsInfo.innerHTML = `Please input a number between 1 and 60(inclusive)!`;
+    }
+    else if(searchBy == "def"){
+      resultsInfo.innerHTML = `Please input a word into the search bar!`;
+    }
+  }
+  else{
+
+  switch(searchBy){
+    case "def":
+      resultsInfo.innerHTML = `Searching for definitions that match "${term}"`;
+      switch(searchType){
+        case "radical":
+          accessByDef(searchType, mainRadicalsArray);
+          break;
+        case "vocabulary":
+          accessByDef(searchType, mainVocabArray);
+          break;
+        case "kanji":
+          accessByDef(searchType, mainKanjiArray);
+          break;
+      }
+      break;
+    case "level":
+      resultsInfo.innerHTML = `Searching for words from level "${term}"`;
+      switch(searchType){
+        case "radical":
+          accessByLevel(searchType, mainRadicalsArray);
+          break;
+        case "vocabulary":
+          accessByLevel(searchType, mainVocabArray);
+          break;
+        case "kanji":
+          accessByLevel(searchType, mainKanjiArray);
+          break;
+      }
+      break;
+    }
+  }
+}
+
+//gets all the kanji from the api
+const getAllWords = (type, initialLink) => {
+  let tempArray = [];
+
+  fetch(new Request(initialLink, {
+    method: 'GET',
+    headers: requestHeaders
+  }))
+    .then(response => response.json())
+    .then(responseBody => {
+      //grabs initial page of information into temporary array
+      tempArray = responseBody.data;
+      //radical information only encompasses one page so it exits early, for the other types,
+      //we have to loop through the rest of the pages
+      if(responseBody.pages.next_url != null){
+        repeatingGetWords(tempArray, type, responseBody.pages.next_url);
+      }
+      else mainRadicalsArray = tempArray;
+    }
+  );
+}
+
+//recursive method that gathers the rest of all the api data and stores them on the page
+const repeatingGetWords = (tempArray, type, nextURL) => {
+    fetch(new Request(nextURL, {
+      method: 'GET',
+      headers: requestHeaders}))
+      .then(response => response.json())
+      .then(responseBody => { 
+        //setting information into a temporary array and getting next page url
+        const emptyArray = tempArray;
+        tempArray = emptyArray.concat(responseBody.data);
+        nextURL = responseBody.pages.next_url;
+        //continues to loop if there is more information to get
+        if(nextURL != null){
+          repeatingGetWords(tempArray, type, nextURL);
+        }
+        else{
+          //setting them to the array that they belong to
+         switch(type){
+          case "kanji":
+            mainKanjiArray = tempArray.slice(0);
+            break;
+          case "vocabulary":
+            mainVocabArray = tempArray.slice(0);
+            break;
+         }
+        }
+      }
+  );
+}
+
+//checks for words that are the user input level, and then gathers them in a great string
+const accessByLevel = (type, array) => {
+  let results = [];
+  let bigString = "";
+
+  for(let i = 0; i< array.length;i++){
+    if(array[i].data.level == term.trim()){
+      results.push(array[i]);
+    }
+  }
+
+  if(utils.hasResults(results, term)){
+    document.querySelector("#num-results").innerHTML = `<p>${results.length} result(s) for level ${term} ${type}</p>`;
+    document.querySelector("#display").innerHTML = getReadings(type, results, bigString);
+  }
+}
+
+//checks for words that contain the user input, and then gathers them in a great string
+const accessByDef = (type, array) => {
+  let results = [];
+  let bigString = "";
+
+  //first checks if the user input term will net any results
+  for(let i = 0; i < array.length;i++){
+    for(let k = 0; k < array[i].data.meanings.length;k++){
+      if(array[i].data.meanings[k].meaning == utils.capitalizeEachWord(term)){
+        results.push(array[i]);
+      }
+    }
+  }
+  //if not then returns nothing and displays that there were no results
+  if(utils.hasResults(results, term)){
+    document.querySelector("#num-results").innerHTML = `<p>${results.length} result(s) for "${term}"</p>`;
+    document.querySelector("#display").innerHTML = getReadings(type, results, bigString);
+  }
+}
+
+//gets all neccesary readings and information for each type
+const getReadings = (type, results, bigString) => {
+switch(type){
+  case "radical":
+    for(let i = 0; i < results.length;i++)
+    {
+      bigString += displayResultsAsString(type, results, i);
+    }
+    return bigString;
+  case "kanji":
+    for(let z = 0; z < results.length;z++){
+      let meaningsString = "";
+  
+      //loops through the meanings data and adds the data to a meanings string
+      let meaningsArray = results[z].data.meanings;
+      for(let j = 0; j < meaningsArray.length;j++){
+        meaningsString += meaningsArray[j].meaning;
+        if(meaningsArray.length > j + 1){
+          meaningsString += ", ";
+        }
+      }
+  
+    let readingsArray = results[z].data.readings;
+  
+    let OnString = "";
+    let KunString = "";
+  
+    //can get both readings by looping through the arrays 
+    for(let h = 0; h < readingsArray.length;h++){
+      if(readingsArray[h].type == "onyomi"){
+        OnString += readingsArray[h].reading;
+        //checks each reading for if the type is onyomi and the next reading in the array as well
+        //then adds them to the onyomi string
+        if(h + 1 < readingsArray.length && readingsArray[h + 1].type == "onyomi"){
+          OnString += ", ";
+        }
+      }
+      //same process for kunyomi as the onyomi readings
+      else if(readingsArray[h].type == "kunyomi"){
+        KunString += readingsArray[h].reading;
+        if(h + 1 < readingsArray.length && readingsArray[h + 1].type == "kunyomi"){
+          KunString += ", ";
+        }
+      }
+    }
+    //some kanji have no readings for onyomi or kunyomi so display none instead
+    if(OnString == ""){
+      OnString = "None";
+    }
+    if(KunString == ""){
+      KunString = "None";
+    }
+      bigString += displayResultsAsString(type, results, z,meaningsString,"", OnString, KunString, );
+    }
+    return bigString;
+  case "vocabulary":
+    for(let z = 0; z < results.length;z++){
+      let meaningsString = "";
+      let readingsString = "";
+  
+      //loops through the meanings data and adds the data to a meanings string
+      let meaningsArray = results[z].data.meanings;
+      for(let j = 0; j < meaningsArray.length;j++){
+        meaningsString += meaningsArray[j].meaning;
+        if(meaningsArray.length > j + 1){
+          meaningsString += ", ";
+        }
+      }
+  
+      //if the type specifically is vocabulary then there is only one reading
+        let readingsArray = results[z].data.readings;
+        for(let j = 0; j < readingsArray.length;j++){
+          readingsString += readingsArray[j].reading;
+          if(readingsArray.length > j + 1){
+            readingsString += ", ";
+        }
+      }
+      bigString += displayResultsAsString(type, results, z, meaningsString, readingsString);
+    }
+    return bigString;
+}
+}
+
+//contains all the ways that each type is represented in a string and returns it to be added to a greater string
+const displayResultsAsString = (type, resultsArray, index ,meanings = "", readings ="", onyomis="", kunyomis="") => {
+  switch(type){
+    case "radical":
+      return `<div class="box is-rounded has-background-danger-light has-text-centered">
+      <p id="identifier">Identifier: ${resultsArray[index].data.meanings[0].meaning}</p>
+      <p id="character">Character: ${resultsArray[index].data.characters}</p>
+      <p id="level">Level: ${resultsArray[index].data.level}</p>
+      </div>`;
+    case "kanji":
+      return `<div class = "box is-rounded has-background-danger-light has-text-centered">
+      <p id="meanings">Meanings: ${meanings}</p>
+      <p id="onyomi">Onyomi: ${onyomis}</p>
+      <p id="kunyomi">Kunyomi: ${kunyomis}</p>
+      <p id="slug">Kanji: ${resultsArray[index].data.characters}</p>
+      <p id="level">Level: ${resultsArray[index].data.level}</p>
+    </div>`;
+    case "vocabulary":
+      return `<div class="box is-rounded has-background-danger-light has-text-centered">
+      <p id="meanings">Meanings: ${meanings}</p>
+      <p id="readings">Kana: ${readings}</p>
+      <p id="slug">Kanji: ${resultsArray[index].data.characters}</p>
+      <p id="level">Level: ${resultsArray[index].data.level}</p>
+    </div>`;
+  }
+}
+
+//adjusts the explanation present each time the type is changed
+const displayExplanation = () => {
+switch(document.querySelector("#type").value){
+  case "radical":
+    info.explainRadicals();
+    break;
+  case "kanji":
+    info.explainKanji();
+    break;
+  case "vocabulary":
+    info.explainVocabulary();
+    break;
+}
 }
--- HW/gao-d-h3-refactor/src/utils.js
+++ HW/gao-d-h3-refactor/src/utils.js
@@ -1,76 +1,32 @@
-const makeColor = (red, green, blue, alpha = 1) => {
-    return `rgba(${red},${green},${blue},${alpha})`;
-};
+const capitalizeEachWord = string => {
+  const sentence = string;
+  const words = sentence.split(" ");
+  const together = words.map(capitalizeFirstLetter);
+  return together.join(" ");
+}
 
-const rainbow = (rotation, alpha = 1) => `hsla(${(rotation * 180 / Math.PI) % 360}, 100%, 50%, ${alpha})`;
-  
-const getRandom = (min, max) => {
-    return Math.random() * (max - min) + min;
-};
-  
-const getRandomColor = () => {
-    const floor = 35; // so that colors are not too bright or too dark 
-    const getByte = () => getRandom(floor,255-floor);
-    return `rgba(${getByte()},${getByte()},${getByte()},1)`;
-};
-  
-const getLinearGradient = (ctx,startX,startY,endX,endY,colorStops) => {
-    let lg = ctx.createLinearGradient(startX,startY,endX,endY);
-    for(let stop of colorStops){
-      lg.addColorStop(stop.percent,stop.color);
+const capitalizeFirstLetter = word => {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+const lowercaseFirstLetter = string => {
+  return string.charAt(0).toLowerCase() + string.slice(1);
+}
+
+//adjusts feedback to user to ensure their search options are configured correctly
+const hasResults = (results,term) => {
+  if(results.length <= 0){
+    if(document.querySelector("#search-by").value == "level" && isNaN(term)){
+      document.querySelector("#num-results").innerHTML = `Please input a NUMBER between 1 and 60(inclusive) in order to search by LEVEL or choose DEFINITION in the search by dropdown.`;
     }
-    return lg;
-};
-
-const drawArrow = (ctx, x, y,rotation, color, width) =>{
-  ctx.save();
-  ctx.translate(x,y);
-  drawLine(ctx, -50,  0,  0, 50, rotation, color, width);
-  drawLine(ctx,   0, 50,  0, 25, rotation, color, width);
-  drawLine(ctx,   0, 25, 50, 25, rotation, color, width);
-  drawLine(ctx,  50, 25, 50,-25, rotation, color, width);
-  drawLine(ctx,  50,-25,  0,-25, rotation, color, width);
-  drawLine(ctx,   0,-25,  0,-50, rotation, color, width);
-  drawLine(ctx,   0,-50,-50,  0, rotation, color, width);
-  ctx.restore();
-}
-
-const scoreLineHighlight = (ctx, startX) =>{
-  drawLine(ctx, startX, 700, startX + 100, 700, "red", 15);
-}
-  
-const drawLine = (ctx, x1, y1,x2,y2, rotation = 0, strokeStyle = "black", lineWidth = 5) =>{
-  ctx.save();
-  ctx.rotate(rotation);
-  ctx.beginPath();
-  ctx.moveTo(x1,y1);
-  ctx.lineTo(x2,y2);
-  ctx.strokeStyle = strokeStyle;
-  ctx.lineWidth = lineWidth;
-  ctx.stroke();
-  ctx.restore();
-}
-
-const drawSquare = (ctx, x,y, width,height) =>{
-  ctx.save();
-  ctx.translate(x,y);
-  ctx.rect(0,0,width,height);
-  ctx.fill();
-  ctx.restore();
-}
-
-  // https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API
-const goFullscreen = (element) => {
-    if (element.requestFullscreen) {
-      element.requestFullscreen();
-    } else if (element.mozRequestFullscreen) {
-      element.mozRequestFullscreen();
-    } else if (element.mozRequestFullScreen) { // camel-cased 'S' was changed to 's' in spec
-      element.mozRequestFullScreen();
-    } else if (element.webkitRequestFullscreen) {
-      element.webkitRequestFullscreen();
+    else if(document.querySelector("#search-by").value == "def" && isNaN(term) == false)
+    document.querySelector("#num-results").innerHTML = `Please input a WORD in order to search by DEFINITION or choose LEVEL in the search by dropdown.`;
+    else{
+    document.querySelector("#num-results").innerHTML = `No results found for "${term}"`;
     }
-    // .. and do nothing if the method is not supported
-};
-  
-export {makeColor, getRandomColor, getLinearGradient, goFullscreen, scoreLineHighlight, drawLine, drawArrow, drawSquare, rainbow};
+    return false;
+  }
+  else return true;
+}
+
+export {capitalizeEachWord, lowercaseFirstLetter, hasResults};
